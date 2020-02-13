1a9883b01a75a6b8b7e6bff23175982d30b76a25
PHPGuus <php.guus@gmail.com>
2020-02-13 16:47:50 +0000

    * Updated release notes link

--- resources/js/components/shared/Footer.vue
+++ resources/js/components/shared/Footer.vue
@@ -1,26 +1,27 @@
 <template>
     <footer class="w-full p-4 flex flex-wrap text-center border-t">
         <div class="w-full sm:pb-2 text-xs md:text-sm lg:text-base sm:border-b sm:border-gray-100">
-            Version 2020.1 - See the Release Notes <a href="">here.</a>
+            Version 2020.1 - See the Release Notes
+            <a href="https://github.com/ProjectManagementDotTech/pmtech/releases" target="_blank">here.</a>
         </div>
         <div class="w-full sm:w-1/2 xl:w-1/3 sm:py-2 text-xs md:text-sm lg:text-base">
-            &copy; 2017 - {{ $moment().format("YYYY") }} by
-            <a href="https://twitter.com/PHPGuus" target="blank">Guus Leeuw jr.
+            &copy; {{ $moment().format("YYYY") }} by
+            <a href="https://twitter.com/PHPGuus" target="_blank">Guus Leeuw jr.
             </a> All rights reserved.
         </div>
         <div class="hidden xl:inline-block w-full xl:w-1/3 sm:py-2 text-xs md:text-sm lg:text-base">
-            Implemented using <a href="https://laravel.com" target="blank">
-            Laravel</a>, <a href="https://vuejs.org" target="blank">Vue.js</a>
-            and <a href="https://tailwindcss.com" target="blank">tailwindcss</a>.
+            Implemented using <a href="https://laravel.com" target="_blank">
+            Laravel</a>, <a href="https://vuejs.org" target="_blank">Vue.js</a>
+            and <a href="https://tailwindcss.com" target="_blank">tailwindcss</a>.
         </div>
         <div class="w-full sm:w-1/2 xl:w-1/3 sm:py-2 text-xs md:text-sm lg:text-base">
-            Operated by <a href="https://www.itpassion.com" target="blank">
+            Operated by <a href="https://www.itpassion.com" target="_blank">
             ITPassion Ltd.</a>
         </div>
         <div class="w-full sm:pt-2 text-xs md:text-sm lg:text-base sm:border-t sm:border-gray-100 xl:hidden">
-            Implemented using <a href="https://laravel.com" target="blank">
-            Laravel</a>, <a href="https://vuejs.org" target="blank">Vue.js</a>
-            and <a href="https://tailwindcss.com" target="blank">tailwindcss</a>.
+            Implemented using <a href="https://laravel.com" target="_blank">
+            Laravel</a>, <a href="https://vuejs.org" target="_blank">Vue.js</a>
+            and <a href="https://tailwindcss.com" target="_blank">tailwindcss</a>.
         </div>
     </footer>
 </template>
